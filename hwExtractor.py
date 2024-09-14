b31e348d7d2f35bb2f94615116b45ffaeb9f0751
ListenToAJ <simaospamm@gmail.com>
2024-09-13 22:34:18 -0400

    Transferred files from old directory

---
+++ hwExtractor.py
@@ -0,0 +1,24 @@
+from pypdf import PdfReader, PdfWriter
+import sys
+
+#~ Variable / Object Initialization
+input_file = sys.argv[1]
+reader = PdfReader(input_file)
+writer = PdfWriter()
+
+searchQuery = "Homework Problem"
+pagesToKeep = []
+
+
+#~ Check every page for search query
+for i in range(len(reader.pages)):
+    if(searchQuery in reader.pages[i].extract_text()):
+        pagesToKeep.append(i)
+
+
+#~ Output
+print('"' + searchQuery + '" found on pages: ' + str(pagesToKeep))
+
+writer.append(reader, pagesToKeep)
+output = open(input_file + " HW Only.pdf", "wb")
+writer.write(output)
